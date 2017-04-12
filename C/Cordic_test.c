6edfe70cf2516c115a117bd968ac465556cc603c
peterscf <floriant.peterschmitt@gmail.com>
2017-04-12 11:31:48 +0200

    .

--- C/Cordic_test.c
+++ C/Cordic_test.c
@@ -1,20 +1,30 @@
 //test Cordic
 
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
+
 
 #define N 6
 #define B 6
 
+#define PI 3.14159265
+ 
 void cellule(int* x, int* y, int* z, int tani, int i);
 
 int main(void){
-
-int x,y,z,i;
+ 
+int x,y,z,i,teta;
+double rad,a_x,a_y;
 int tan0[] ={45,26,14,7,3,1};
- z=0;
- x=(int)(0.5*(0x1<<B));
- y=(int)(-0.86*(0x1<<B));
+
+  z=0;
+  for (teta = -90; teta< 90; teta +=2){
+	
+	rad = teta*PI/180;	
+ 	a_x = cos (rad);
+	a_y = sin (rad);
+	//x= (int)(a_x*(0x1<<B));
+ 	//y= (int)(a_y*(0x1<<B));
 	
 	printf("lancement du test N=%d\n",N);
 	for (i=0;i<N;i++){
@@ -22,7 +32,7 @@ int tan0[] ={45,26,14,7,3,1};
 	}
 	//printf("valeur de sortie z: Ox%x \n",z);
 	printf("valeur de sortie z: %d \n",z);
-
+  }
 }
 
 void cellule(int* x, int* y, int* z, int tani, int i){
@@ -31,7 +41,7 @@ void cellule(int* x, int* y, int* z, int tani, int i){
 	int yk=*y;
 	int zk=*z;
 	
-	printf("Entree:\nxk=%d \nyk=%d \n zk=%d\n" ,xk,yk, zk);
+	//printf("Entree:\nxk=%d \nyk=%d \n zk=%d\n" ,xk,yk, zk);
 	if (yk > 0){
 		*x=xk+(yk>>i);
 		*y=yk-(xk>>i);
@@ -42,6 +52,6 @@ void cellule(int* x, int* y, int* z, int tani, int i){
 		*y=yk+(xk>>i);
 		*z=zk-tani;
 	}
-	printf("Sortie:\nx=%d \ny=%d \n z=%d\n" ,*x,*y, *z);
+	//printf("Sortie:\nx=%d \ny=%d \n z=%d\n" ,*x,*y, *z);
 
 }
