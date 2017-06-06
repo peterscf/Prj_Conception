526490cba2d15d9dda76fba280a446eb969ab8b4
peterscf <floriant.peterschmitt@gmail.com>
2017-06-06 15:11:52 +0200

    ajout du zip SCRIPTS.zip

--- Design/spy/DUMMY/SCRIPTS/defines.tcl
+++ Design/spy/DUMMY/SCRIPTS/defines.tcl
@@ -2,7 +2,8 @@
 set PROJECT_PATH "/tp/xph2app/xph2app003/Prj_conception/Design/"
 set WORK_PATH "spy/WORK/"
 
-set VHDL_FILES "../../vhd/cell.vhd ../../vhd/cordic.vhd"
+set VHDL_FILES "../../vhd/cell.vhd \
+				../../vhd/cordic.vhd"
 					
 set DESIGN_NAME CORDIC_top
 
---
+++ Design/spy/DUMMY/SCRIPTS/init.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo creation du rep  WORK
+mkdir ./../WORK
+echo copy du run_spy.sh
+cp ./run_spy.sh ./../WORK/
+echo creation du rep. REPORTS
+mkdir ./../REPORTS
+cd ./../WORK
+echo lancer le script: ./run_spy.sh
+echo ENJOIE!!!
