9223cae3f331c50b7a5ed24178579d00ad6bd718
peterscf <floriant.peterschmitt@gmail.com>
2017-06-07 17:33:43 +0200

    ajour pre_process.vhd et dans le bench + bench pre_process

--- Design/vhd/cordic.vhd
+++ Design/vhd/cordic.vhd
@@ -8,13 +8,24 @@ entity CORDIC_top is
 
 	port( 	clk  : in std_logic;
 		reset_n  : in std_logic;
-	  	X  : in  std_logic_vector(7 downto 0);
-		Y  : in  std_logic_vector(7 downto 0);
-		Z  : out std_logic_vector(7 downto 0));
+	  	X  : in  std_logic_vector(Nb-1 downto 0);
+		Y  : in  std_logic_vector(Nb-1 downto 0);
+		Z  : out std_logic_vector(8 downto 0));
 end CORDIC_top;
 
 architecture A of CORDIC_top is
-
+component PRE_PROCESS
+	generic(  Nb: integer );
+  	
+	port( 	clk : in std_logic;
+			reset_n : in std_logic;
+			Iin  : in std_logic_vector(Nb-1 downto 0);
+			Qin  : in std_logic_vector(Nb-1 downto 0);
+	  		Iout : out  std_logic_vector(Nb-1 downto 0);
+			Qout : out  std_logic_vector(Nb-1 downto 0);
+			Z : out std_logic_vector(8 downto 0));
+  end component;
+  
   component CELL
 	  generic( Nb:integer; --Nb bits du bus en entrée 
 	  tan_i: integer; --angle
@@ -23,45 +34,57 @@ architecture A of CORDIC_top is
 
     port(   clk: in std_logic;
             reset_n : in std_logic;
-            x_in: in std_logic_vector(7 downto 0);
-            y_in: in std_logic_vector(7 downto 0);
-            z_in: in std_logic_vector(7 downto 0);
-            x_out: out std_logic_vector(7 downto 0);         
-            y_out: out std_logic_vector(7 downto 0);
-            z_out: out std_logic_vector(7 downto 0));
+            x_in: in std_logic_vector(Nb-1 downto 0);
+            y_in: in std_logic_vector(Nb-1 downto 0);
+            z_in: in std_logic_vector(8 downto 0);
+            x_out: out std_logic_vector(Nb-1 downto 0);         
+            y_out: out std_logic_vector(Nb-1 downto 0);
+            z_out: out std_logic_vector(8 downto 0));
   end component;
 
-  signal Z0	: std_logic_vector(7 downto 0);
+  signal Z0	: std_logic_vector(Nb-1 downto 0);
+  signal X0 : std_logic_vector(Nb-1 downto 0);
+  signal Y0	: std_logic_vector(8 downto 0);
 
-  signal X1 	: std_logic_vector(7 downto 0);
-  signal Y1	: std_logic_vector(7 downto 0);
-  signal Z1	: std_logic_vector(7 downto 0);
+  signal X1 	: std_logic_vector(Nb-1 downto 0);
+  signal Y1	: std_logic_vector(Nb-1 downto 0);
+  signal Z1	: std_logic_vector(8 downto 0);
  
-  signal X2 	: std_logic_vector(7 downto 0);
-  signal Y2	: std_logic_vector(7 downto 0);
-  signal Z2	: std_logic_vector(7 downto 0);
+  signal X2 	: std_logic_vector(Nb-1 downto 0);
+  signal Y2	: std_logic_vector(Nb-1 downto 0);
+  signal Z2	: std_logic_vector(8 downto 0);
 
-  signal X3 	: std_logic_vector(7 downto 0);
-  signal Y3	: std_logic_vector(7 downto 0);
-  signal Z3	: std_logic_vector(7 downto 0);
+  signal X3 	: std_logic_vector(Nb-1 downto 0);
+  signal Y3	: std_logic_vector(Nb-1 downto 0);
+  signal Z3	: std_logic_vector(8 downto 0);
 
-  signal X4 	: std_logic_vector(7 downto 0);
-  signal Y4	: std_logic_vector(7 downto 0);
-  signal Z4	: std_logic_vector(7 downto 0);
+  signal X4 	: std_logic_vector(Nb-1 downto 0);
+  signal Y4	: std_logic_vector(Nb-1 downto 0);
+  signal Z4	: std_logic_vector(8 downto 0);
 
-  signal X5 	: std_logic_vector(7 downto 0);
-  signal Y5	: std_logic_vector(7 downto 0);
-  signal Z5	: std_logic_vector(7 downto 0);
+  signal X5 	: std_logic_vector(Nb-1 downto 0);
+  signal Y5	: std_logic_vector(Nb-1 downto 0);
+  signal Z5	: std_logic_vector(8 downto 0);
 
-  signal X6 	: std_logic_vector(7 downto 0);
-  signal Y6	: std_logic_vector(7 downto 0);
-  signal Z6	: std_logic_vector(7 downto 0);
+  signal X6 	: std_logic_vector(Nb-1 downto 0);
+  signal Y6	: std_logic_vector(Nb-1 downto 0);
+  signal Z6	: std_logic_vector(8 downto 0);
 
 
 begin
 
 	Z <= Z6;
-	Z0<= "00000000";
+
+pp1: PRE_PROCESS generic map ( Nb => 8)
+  port map (
+  	clk,
+    	reset_n,
+    	X,
+   		Y,
+       	X0,
+    	Y0,
+    	Z0);	
+	
   cell1 : CELL generic map ( Nb => 8,
 		tan_i => 45,
 		shift => 0 )
@@ -70,7 +93,7 @@ begin
     	reset_n,
     	X,
    	Y,
-	Z0,
+		Z0,
        	X1,
     	Y1,
     	Z1);
---
+++ Design/vhd/pre_process.vhd
@@ -0,0 +1,61 @@
+-----------------------------PRE_PROCESS.vhd----------------------------------------
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity PRE_PROCESS is
+
+	generic(  Nb: integer );
+  	
+	port( 	clk : in std_logic;
+			reset_n : in std_logic;
+			Iin  : in std_logic_vector(Nb-1 downto 0);
+			Qin  : in std_logic_vector(Nb-1 downto 0);
+	  		Iout : out  std_logic_vector(Nb-1 downto 0);
+			Qout : out  std_logic_vector(Nb-1 downto 0);
+			Z : out std_logic_vector(8 downto 0));
+end PRE_PROCESS;
+
+architecture A of PRE_PROCESS is
+
+	signal sig_Iout:signed(Nb-1 downto 0);
+	signal sig_Qout:signed(Nb-1 downto 0);
+	signal sig_Zout: std_logic_vector(8 downto 0);
+
+begin 
+
+  synchro: process (clk,reset_n)
+  begin
+	if clk'event and clk ='1' then 
+		if(reset_n = '0')then 
+  			Z<="000000000";
+  			Iout<="00000000";
+  			Qout<="00000000";
+	else 
+ 			Z <= sig_Zout;
+ 			Iout<= std_logic_vector(sig_Iout);
+ 			Qout<= std_logic_vector(sig_Qout);
+		end if;
+	end if;
+
+  end process synchro;
+
+
+  compute: process (Iin,Qin)
+  begin 
+	if(Iin(Nb-1)='1') then
+		sig_Iout <= not(signed(Iin))+1;
+		sig_Qout <= not(signed(Qin))+1;			
+		if(Qin(Nb-1)='1') then 
+			sig_Zout<= "010110100";-- +180°
+		else 
+			sig_Zout<= "101001100";-- -180°
+		end if;-- Qin > 0 
+	else 
+		sig_Iout <= signed(Iin);
+		sig_Qout <= signed(Qin);
+		sig_Zout <= "000000000";
+	end if;
+  end process compute;
+	
+end A;
