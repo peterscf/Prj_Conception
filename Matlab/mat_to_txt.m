fe772f46c9447a17ae7d5593662b6bae2eaf78ce
peterscf <floriant.peterschmitt@gmail.com>
2017-06-07 17:37:10 +0200

    ajout des fichier matlab

--- Matlab/mat_to_txt.m
+++ Matlab/mat_to_txt.m
@@ -11,3 +11,7 @@ dlmwrite('BitStream.txt', (Data2.(DataField2{1})'));
 Data3 = load('Qout.mat');
 DataField3 = fieldnames(Data3);
 dlmwrite('Qout.txt', (Data3.(DataField3{1})'));
+
+Data4 = load('phase.mat');
+DataField4 = fieldnames(Data4);
+dlmwrite('Phase.txt', (Data4.(DataField4{1})'));
